ae28b3fc525ccd96669de9df41164746d1c80322
rebeca018 <rebecasnascimento018@gmail.com>
2023-08-25 14:27:18 -0400

    Commit

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,12 +2,42 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
-   
+   const [atividade, setAtividade] = useState("");
+   const [lista, setLista] = useState([]);
+   const [id, setId] = useState(1);
+
+   const salvar = (e) => {
+        e.preventDefault();
+        setLista([...lista, {
+            atividade: atividade,
+            id: id
+        }]);
+        setId(id + 1);
+        setAtividade("");
+   };
+
+
     return (
         <div>
-            <h1>Lista de Atividades</h1>
             <Link to="/">home</Link>
+            <h1>Lista de Atividades</h1>
+
+            <p>{atividade}</p>
+
+            <form onSubmit={salvar}>
+                <input value={atividade} type="text"
+                onChange={(e)=>{ setAtividade(e.target.value)}} />
+                <button>ADD</button>
+                <button ></button>
+            </form>
+
+            {lista.map((ativ)=>
+            <div key={ativ.id}>
+                <p>{ativ.atividade}</p>
+            </div>)}
            
         </div>
     );
 }
+
+//filter id diferente 
