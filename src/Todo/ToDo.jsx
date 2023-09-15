dc7438b85214e9b2c69e872e040cf57f069d6e91
rebeca018 <rebecasnascimento018@gmail.com>
2023-09-15 13:50:40 -0400

    Commit input titulo

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
-   const [atividade, setAtividade] = useState("");
+   //const [atividade, setAtividade] = useState("");
+   const [titulo, setTitulo] = useState("");
    const [lista, setLista] = useState([]);
    const [id, setId] = useState(1);
 
@@ -21,7 +22,7 @@ export default function ToDo() {
         const auxLista = [];
         lista.map((lista) => {
             if (lista.id !== id){
-                auxLista.push(Lista);
+                auxLista.push(lista);
             }
         });
         setLista(auxLista);
@@ -34,19 +35,23 @@ export default function ToDo() {
             <Link to="/">home</Link>
             <h1>Lista de Atividades</h1>
 
-            <p>{atividade}</p>
+            <p>{titulo}</p>
 
             <form onSubmit={salvar}>
+                <input value={titulo} type="text"
+                onChange={(e)=>{ setAtividade(e.target.value)}} />
+                <input value={atividade} type="text"
+                onChange={(e)=>{ setAtividade(e.target.value)}} />
                 <input value={atividade} type="text"
                 onChange={(e)=>{ setAtividade(e.target.value)}} />
                 <button>ADD</button>
-                <button>EXCLUIR</button>
             </form>
 
             {lista.map((ativ)=>
-            <div key={ativ.id}>
-                <p>{ativ.atividade}</p>
-            </div>)}
+            <ul key={ativ.id}>
+                <li>{ativ.titulo}</li>
+                <button onClick={() => apagar(ativ.id)}>Remover</button>
+            </ul>)}
            
         </div>
     );
