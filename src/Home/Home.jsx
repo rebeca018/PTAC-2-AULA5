57a02d21ba3db47d2a396f306a2a126f0cddad3a
rebeca018 <rebecasnascimento018@gmail.com>
2023-09-19 09:24:49 -0400

    commit estilização home e link bootstrap

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,9 +6,17 @@ export default function Home() {
         <div class="home">
             <img src="/imagem/capa.png" alt="" />
             <div class="home">
-                <p class="texto">testando</p>
+                <h3 class="texto-home">Bem-vindo(a) à minha biblioteca! Abaixo está um resumo dos meus livros favoritos e aqui ... está a minha wishlist , a minha lista de livros que desejo comprar</h3>
+
+                <div>
+                    <img src="https://m.media-amazon.com/images/I/71P-Mok4cqL._AC_UL320_.jpg" alt="" class="book-card-img"/>
+                    <p class="book-card-p">Estilhaça-me</p> 
+                    <p class="book-card-p">Thareh Mafi</p>
+                    <p class="book-card-p">Universo dos livros</p>
+                </div>
+
             </div>
-            <Link to="/todo">todo</Link>
+           <h2><Link to="/todo">todo</Link></h2>
         </div>
 
     );
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './Home/Home';
 import ToDo from './Todo/ToDo';
+import "./Todo/style.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
