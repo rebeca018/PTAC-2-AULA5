50fcd1afadbbf05742cf08e8db1c86091e4925f3
rebeca018 <rebecasnascimento018@gmail.com>
2023-11-17 14:25:15 -0400

    Commit componentes

---
+++ src/Componentes/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card({livro}){
+    return(
+        <div>
+            <h1>{livro.autor}</h1>
+        </div>
+    );
+}
--- src/Detalhe/detalhe.jsx
+++ src/Detalhe/detalhe.jsx
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom"
+import Card from '../Componentes/Card';
 
 export default function Detalhe(){
    const { id } = useParams();
-   
+   const lista = JSON.parse(localStorage.getItem("Lista"()));
+   console.log(lista);
+
+    const livro = lista.filter((objeto) => {
+         if(objeto.id == id){
+            return objeto;
+         }
+         return null;
+    })
+
+    console.log(livro[0]);
+
     return(
-        <div>
-            <h1>Deu certo {id} </h1>
-        </div>
+        <Card livro={livro[0]} />
     )
 }
